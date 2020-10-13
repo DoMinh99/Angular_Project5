e7e3f688dbe738b7c7167c9df38c0ff41fed7fa7
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-13 13:23:29 +0700

    up

---
+++ Admin/src/app/base/base-component.ts
@@ -0,0 +1,47 @@
+//import { CartService } from './cart.service';
+import { Injector, Renderer2 } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { of as observableOf, Subject } from 'rxjs';
+//import { ApiService } from './api.service';
+export class BaseComponent {
+   public unsubscribe = new Subject();
+   public _renderer:any;
+   //public _api: ApiService;
+   //public _cart: CartService;
+   public _route: ActivatedRoute;
+   constructor(injector: Injector) {  
+      this._renderer = injector.get(Renderer2);
+     // this._api = injector.get(ApiService);
+      //this._cart = injector.get(CartService);
+      this._route = injector.get(ActivatedRoute);
+      } 
+   public loadScripts() 
+   {
+         this.renderExternalScript('"assets/js/libs/jquery-3.1.1.min.js"').onload = () => {
+         }
+         this.renderExternalScript('assets/bootstrap/js/popper.min.js').onload = () => {
+        }
+        this.renderExternalScript(' assets/bootstrap/js/bootstrap.min.js').onload = () => {
+        }
+        this.renderExternalScript(' assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js').onload = () => {
+        }
+        this.renderExternalScript('assets/js/app.js').onload = () => {
+         }
+         this.renderExternalScript('assets/js/custom.js').onload = () => {
+         }
+         this.renderExternalScript('assets/plugins/apex/apexcharts.min.js').onload = () => {
+        }
+        this.renderExternalScript('assets/js/dashboard/dash_1.js').onload = () => {
+        }  
+    }
+   public renderExternalScript(src: string): HTMLScriptElement
+    {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = src;
+        script.async = true;
+        script.defer = true;
+        this._renderer.appendChild(document.body, script);
+        return script;
+    }
+}
