9347b32c2c98499048588f226971d1d008e02ec9
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-16 21:03:32 +0700

    update getData

--- Eshop/src/app/app.component.ts
+++ Eshop/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MyserviceService } from './myservice.service';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Eshop';
+  title = 'Angular 7 Project!'; 
+  public persondata = [];
+  constructor(private myservice: MyserviceService) {}
+  ngOnInit() {
+     this.myservice.getData().subscribe((data) => {
+        Object.keys(data).forEach(key => {
+          this.persondata.push(data[key]);
+        });
+        console.log(this.persondata);
+     });
+  }
 }
