be49cbd443db5d7438be26bb5fbbb8bb7862d8e0
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-17 20:46:14 +0700

    update

--- Eshop/src/app/app-routing.module.ts
+++ Eshop/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+import { bodyComponent } from './body/body.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { CartComponent } from './cart/cart.component';
 import { ContactComponent } from './contact/contact.component';
@@ -15,7 +15,7 @@ import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [ 
   {path:"checkout", component:CheckoutComponent}, 
-  {path:"", component:BodyComponent}, 
+  {path:"", component:bodyComponent}, 
   {path:"Login", component:LoginComponent}, 
   {path:"productDetail", component:ProductDetailComponent}, 
   {path:"contact", component:ContactComponent}, 
