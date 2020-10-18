e1522bf8497909c6f041eb6901550957a872f917
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-18 11:08:31 +0700

    create component

--- Eshop/src/app/app.module.ts
+++ Eshop/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { BodyComponent } from './body/body.component';
 @NgModule({
   declarations: [
     AppComponent,
-    
     HeaderComponent,
     FooterComponent,
     CheckoutComponent,
--- Eshop2/src/app/app.module.ts
+++ Eshop2/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SharedComponent } from './shared/shared.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { SeachComponent } from './seach/seach.component';
+import { SearchComponent } from './search/search.component';
+import { FoodterComponent } from './shared/foodter/foodter.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { CategoryComponent } from './shared/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SharedComponent,
+    LoginComponent,
+    AboutComponent,
+    ProductComponent,
+    SeachComponent,
+    SearchComponent,
+    FoodterComponent,
+    HeaderComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
