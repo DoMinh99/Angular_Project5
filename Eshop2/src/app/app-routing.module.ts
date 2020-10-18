1af5522f4f833f0b5a354515d4f98ab60d4391d6
dohongminh2811999@gmail.com <57739580+DoMinh99@users.noreply.github.com>
2020-10-18 17:11:50 +0700

    update

--- Eshop2/src/app/app-routing.module.ts
+++ Eshop2/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { SharedComponent } from './shared/shared.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CategoryComponent } from './shared/category/category.component';
+
+const routes: Routes = [
+  { path:'', component:MainComponent },
+  { path:'about', component:AboutComponent },
+  { path:'shared', component:SharedComponent },
+  { path:'login', component:LoginComponent },
+  { path:'product', component:ProductComponent },
+  { path:'search', component:SearchComponent },
+  { path:'header', component:HeaderComponent },
+  { path:'footer', component:FooterComponent },
+  { path:'category', component:CategoryComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Eshop2/src/app/app.module.ts
+++ Eshop2/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { SharedComponent } from './shared/shared.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CategoryComponent } from './shared/category/category.component';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    SharedComponent,
+    LoginComponent,
+    ProductComponent,
+    SearchComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
